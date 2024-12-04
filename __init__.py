588438d5b379e52cd790e122a69e4ef8db403f20
chengzeyi <ichengzeyi@gmail.com>
2024-12-04 15:08:17 +0800

    support quantizing clip

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
-from .misc_nodes import EnhancedDiffusionModelLoader
-from .velocator_nodes import VelocatorQuantizeModel, VelocatorCompileModel
+from .misc_nodes import EnhancedLoadDiffusionModel
+from .velocator_nodes import (
+    VelocatorLoadAndQuantizeDiffusionModel,
+    VelocatorLoadAndQuantizeClip,
+    VelocatorQuantizeModel,
+    VelocatorCompileModel,
+)
 
 
 def patch_cast_to():
+
     def cast_to(weight, dtype=None, device=None, non_blocking=False, copy=False):
         if device is None or weight.device == device:
             if not copy:
@@ -13,9 +19,7 @@ def patch_cast_to():
         # torch.empty_like does not work with tensor subclasses well
         # r = torch.empty_like(weight, dtype=dtype, device=device)
         # r.copy_(weight, non_blocking=non_blocking)
-        r = weight.to(
-            device=device, dtype=dtype, non_blocking=non_blocking, copy=copy
-        )
+        r = weight.to(device=device, dtype=dtype, non_blocking=non_blocking, copy=copy)
         return r
 
     from comfy import model_management
@@ -23,16 +27,20 @@ def patch_cast_to():
     model_management.cast_to = cast_to
 
 
-# patch_cast_to()
+patch_cast_to()
 
 NODE_CLASS_MAPPINGS = {
-    "EnhancedDiffusionModelLoader": EnhancedDiffusionModelLoader,
+    "EnhancedLoadDiffusionModel": EnhancedLoadDiffusionModel,
+    "VelocatorLoadAndQuantizeDiffusionModel": VelocatorLoadAndQuantizeDiffusionModel,
+    "VelocatorLoadAndQuantizeClip": VelocatorLoadAndQuantizeClip,
     "VelocatorQuantizeModel": VelocatorQuantizeModel,
     "VelocatorCompileModel": VelocatorCompileModel,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "EnhancedDiffusionModelLoader": "Load Diffusion Model+",
-    "VelocatorQuantizeModel": "Velocator Quantize Model",
-    "VelocatorCompileModel": "Velocator Compile Model",
+    "EnhancedLoadDiffusionModel": "Load Diffusion Model+",
+    "VelocatorLoadAndQuantizeDiffusionModel": "🚀Load & Quantize Diffusion Model",
+    "VelocatorLoadAndQuantizeClip": "🚀Load & Quantize CLIP",
+    "VelocatorQuantizeModel": "🚀Quantize Model",
+    "VelocatorCompileModel": "🚀Compile Model",
 }
---
+++ utils.py
@@ -0,0 +1,49 @@
+import contextlib
+import unittest
+import torch
+
+
+def get_weight_dtype_inputs():
+    return {
+        "weight_dtype": (
+            [
+                "default",
+                "float32",
+                "float64",
+                "bfloat16",
+                "float16",
+                "fp8_e4m3fn",
+                "fp8_e4m3fn_fast",
+                "fp8_e5m2",
+            ],
+        ),
+    }
+
+
+def parse_weight_dtype(model_options, weight_dtype):
+    dtype = {
+        "float32": torch.float32,
+        "float64": torch.float64,
+        "bfloat16": torch.bfloat16,
+        "float16": torch.float16,
+        "fp8_e4m3fn": torch.float8_e4m3fn,
+        "fp8_e4m3fn_fast": torch.float8_e4m3fn,
+        "fp8_e5m2": torch.float8_e5m2,
+    }.get(weight_dtype, None)
+    if dtype is not None:
+        model_options["dtype"] = dtype
+    if weight_dtype == "fp8_e4m3fn_fast":
+        model_options["fp8_optimizations"] = True
+    return model_options
+
+
+@contextlib.contextmanager
+def disable_load_models_gpu():
+
+    def foo(*args, **kwargs):
+        pass
+
+    from comfy import model_management
+
+    with unittest.mock.patch.object(model_management, "load_models_gpu", foo):
+        yield
